ae3a379a5bf5f7f5d82f5e051024a51eff9e9ba1
SonicHsu <soniccz@gmail.com>
2025-07-01 21:53:27 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import "./style.css";
+import { ref } from "vue";
+const count = ref(0);
+</script>
+
+<template>
+  <h1 class="text-3xl">{{ count }}</h1>
+  <header>
+    <nav>
+      <button
+        class="bg-blue-400 rounded-xl px-2 cursor-pointer text-white"
+        @click="count--"
+      >
+        Button01
+      </button>
+      <button
+        class="bg-blue-400 rounded-xl px-2 cursor-pointer text-white"
+        @click="count++"
+      >
+        Button01
+      </button>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
